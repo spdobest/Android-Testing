bbac4eee50e5d57fa76c738caf7f9884057e7b10
Sibaprasad <sibaprasad.X.mohanty@aib.ie>
2020-02-10 16:46:37 +0000

    Added hide keyboard option

--- app/src/main/java/com/spm/androidtesting/account/OnboardingTestFragment.kt
+++ app/src/main/java/com/spm/androidtesting/account/OnboardingTestFragment.kt
@@ -1,13 +1,19 @@
 package com.spm.androidtesting.account
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.navigation.fragment.NavHostFragment.findNavController
-import com.spm.androidtesting.R
 import kotlinx.android.synthetic.main.fragment_onboarding_test.*
+import sun.jvm.hotspot.utilities.IntArray
+
 
 class OnboardingTestFragment : Fragment() {
 
@@ -38,8 +44,34 @@ class OnboardingTestFragment : Fragment() {
         buttonRegister?.setOnClickListener {
             findNavController(this).navigate(R.id.registerTestFragment, null)
         }
+
+
+        buttonShowHide.setOnClickListener {
+            hideKeyboard(activity as FragmentActivity)
+        }
+
     }
 
+    fun hideKeyboard(activity: Activity) {
+        val imm: InputMethodManager =
+            activity.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        //Find the currently focused view, so we can grab the correct window token from it.
+        var view = activity.currentFocus
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = View(activity)
+        }
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    fun hideKeyBoard() {
+        val view1: View = activity.getCurrentFocus()
+        if (view != null) {
+            val imm =
+                getSystemService<Any>(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+            imm!!.hideSoftInputFromWindow(view1.windowToken, 0)
+        }
+    }
 
     companion object {
         // TODO: Rename and change types and number of parameters
